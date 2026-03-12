e2727af5e6011912dab816f3b085c79e7a7dc0e7
barrera-daniel-jpg <barreradb77@gmail.com>
2026-03-11 19:57:23 -0500

    importacion de las funciones

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-#Creacion del main
+from History import show_summary
+from Login import product_records
+
+print("="*24)
+print("| WELCOME TO THE SYSTEM |")
+print("="*24)
+
+def request_option():
+    opcion = input("\nAre you going to register products? (Y/N): ").upper()
+    if opcion not in ['Y', 'N']:
+        print(" Invalid option, please try again (Y/N)")
+        return request_option()  
+    return opcion
+
+while request_option() == 'Y':
+    product_records()
+show_summary()
+print("The program has ended. Thank you for your registrations.")

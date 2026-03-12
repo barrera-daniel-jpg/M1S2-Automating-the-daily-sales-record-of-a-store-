5c2d0267c0c218eb280c5b06ae10e71ac94c03db
barrera-daniel-jpg <barreradb77@gmail.com>
2026-03-12 17:12:18 -0500

    comentarios

--- Login.py
+++ Login.py
@@ -1,31 +1,33 @@
-from History import add_to_history
+from History import add_to_history #Trae add_to_history de History.py y activa la lista global
 
 def product_records():
     
-    product = input(">> Please enter the item to register: ").capitalize()
+    #Usa .capitalize() para estandarizar la entrada del usuario
+    product = input(">> Please enter the item to register: ").capitalize() 
     try:
         price = float(input(">> Please enter the product price: "))
         quantity = int(input(">> Please enter the quantity of products sold: "))
-    except ValueError:
+    except ValueError: #Captura ValueError y reinicia el proceso si hay texto inválido
         print("_"*40)
         print("\nInvalid data. Please enter numbers only.")
         print("_"*40)
         return product_records()
-    total = price*quantity
+    total = price*quantity #Calcula el total de la operacion
     
 
     
-    
+   #Arma diccionario con 4 datos 
     producto = {
         "product": product,
         "price": price,
         "quantity": quantity,
         "total": total
     }
-    add_to_history(producto)
+    add_to_history(producto) # Llama a add_to_history(producto) para guardar el paquete en la lista global, luego imprime una confirmación
+# al usuario con el nombre del producto y el total.
     
-    print("_"*40)
+    print("_"*40) # Mejora la visualizacion y la experiencia de usuario
     print(f"\n>> {producto['product']} It was correctly registered")
     print(f">> The total amount raised was: ${producto['total']}")
-    print("_"*40)
+    print("_"*40) # Mejora la visualizacion y la experiencia de usuario
     

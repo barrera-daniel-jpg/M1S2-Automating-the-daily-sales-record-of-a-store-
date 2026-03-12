401de3a7f86106770bc7c6408ed164e684cbd1d2
barrera-daniel-jpg <barreradb77@gmail.com>
2026-03-11 19:51:39 -0500

    creacion del codigo

--- History.py
+++ History.py
@@ -1,1 +1,31 @@
-#Creacion del historial
+from History import add_to_history
+
+def product_records():
+    
+    product = input(">> Please enter the item to register: ").capitalize()
+    try:
+        price = float(input(">> Please enter the product price: "))
+        quantity = int(input(">> Please enter the quantity of products sold: "))
+    except ValueError:
+        print("_"*40)
+        print("\nInvalid data. Please enter numbers only.")
+        print("_"*40)
+        return product_records()
+    total = price*quantity
+    
+
+    
+    
+    producto = {
+        "product": product,
+        "price": price,
+        "quantity": quantity,
+        "total": total
+    }
+    add_to_history(producto)
+    
+    print("_"*40)
+    print(f"\n>> {producto['product']} It was correctly registered")
+    print(f">> The total amount raised was: ${producto['total']}")
+    print("_"*40)
+    
